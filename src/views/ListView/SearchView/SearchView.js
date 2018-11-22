49ed10db9f10f15cd9f11e67b883e61d33cf780f
Karol szykula <karol.szykula@gmail.com>
2018-11-22 15:37:18 +0100

    Wip

--- src/views/ListView/SearchView/SearchView.js
+++ src/views/ListView/SearchView/SearchView.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import Paper from 'material-ui/Paper'
+
+const style = {
+    height: 100,
+    width: 100,
+    margin: 20,
+    textAlign: 'center',
+    display: 'inline-block',
+}
+
 const SearchView = (props) => (
-    <div>
-        SearchView
-    </div>
+    <Paper>
+asdf
+    </Paper>
 )
 
 export default SearchView
