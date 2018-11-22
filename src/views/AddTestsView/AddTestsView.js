7ffac5c7e3929efa54c6fef4312b48db381534c9
AleksanderSzczuchniak <alex.szczuchniak@gmail.com>
2018-11-22 16:55:08 +0100

    kolejna linijka

--- src/views/AddTestsView/AddTestsView.js
+++ src/views/AddTestsView/AddTestsView.js
@@ -4,7 +4,7 @@ class AddTestView {
     render() {
         return (
             <div className="container">
-                <h3>AddTestView/h3>
+                <h3>AddTestView</h3>
                 <AddTestContainer />
             </div>
         )
