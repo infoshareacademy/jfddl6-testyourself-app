daf2a7187327d1262b77c0210379db68c4712a88
Andrzej Rokosz <andrzej_ro@wp.pl>
2018-11-23 16:55:30 +0100

    wip

--- src/Test.js
+++ src/Test.js
@@ -1,22 +1,12 @@
-const data = [{
-    category: 'Geography',
-    question: 'What is the capital city of Poland?',
-    answers: ['Warsaw', 'Cracow', 'Lublin'],
-    rightAnswer: 'Warsaw',
-    isFavorite: false
-},
-{
-    category: 'Chemistry',
-    question: 'What is the lightest element?',
-    answers: ['Carbon', 'Hydrogen', 'Iron', 'Sulphur'],
-    rightAnswer: 'Hydrogen',
-    isFavorite: false
-},
-{
-    category: 'Biology',
-    question: 'How many bones do adult people have?',
-    answers: ['204', '206', '208', '212', '209'],
-    rightAnswer: '206',
-    isFavorite: false
-}
-]
+import React from 'react'
+import { ListItem } from 'material-ui/List'
+
+
+const Test = (props) => (
+    <ListItem>
+    aaaa
+        
+    </ListItem>
+)
+
+export default Test
