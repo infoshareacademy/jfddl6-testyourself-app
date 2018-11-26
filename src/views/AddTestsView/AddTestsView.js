4e7d603a0b158282399731cab189629624e4081d
AleksanderSzczuchniak <alex.szczuchniak@gmail.com>
2018-11-26 16:01:33 +0100

    created conteners

--- src/views/AddTestsView/AddTestsView.js
+++ src/views/AddTestsView/AddTestsView.js
@@ -1,20 +1,28 @@
 import React from 'react'
+import Paper from "material-ui/Paper"
+import { Grid, Row, Col } from "react-flexbox-grid"
+import SelectField from "material-ui/SelectField"
+import RaisedButton from "material-ui/RaisedButton"
+import TextField from "material-ui/TextField"
+import MenuItem from "material-ui/MenuItem"
 
-const categories = ["categories", "question", "answers","correctAnswer",]
+
+
+const categories = ["Select Category", "Select Difficulty", "Select Type", "Question", "Answers", "CorrectAnswer"]
 
 const style = {
     paper: {
-      margin: 6,
-      padding: 20
+        margin: 6,
+        padding: 20
     },
     button: {
-      marginTop: 20
+        marginTop: 20
     },
     item: {
-      float: "left"
+        float: "left"
     }
-  };
-  
+};
+
 
 class AddTestView extends Component {
 
@@ -24,7 +32,84 @@ class AddTestView extends Component {
     handlerChange = (event, index, value) => this.setState({ value: value })
 
     render() {
-        return ({})
+        return (
+            <Paper
+                style={style.paper}>
+                <Grid fluid>
+                    <Row
+                        center="xs">
+                        <Col lg={10} />
+                        <h1>
+                            Add Test
+                        </h1>
+                    </Row>
+                    <div>
+
+                        <Row center="xs">
+                            <Col lg={10} />
+                            <TextField hintText="Select Category"
+                                fullWidth={true} />
+                        </Row>
+                        <Row center="xs">
+                            <Col lg={10} />
+                            <TextField hintText="Select Difficulty"
+                                fullWidth={true} />
+                        </Row>
+                        <Row center="xs">
+                            <Col lg={10} />
+                            <TextField hintText="Select Type"
+                                fullWidth={true} />
+                        </Row>
+                        <Row center="xs">
+                            <Col lg={10} />
+                            <TextField hintText="Question"
+                                fullWidth={true} />
+                        </Row>
+                        <Row center="xs">
+                            <Col lg={10} />
+                            <TextField hintText="Answers"
+                                fullWidth={true} />
+                        </Row>
+                        <Row center="xs">
+                            <Col lg={10} />
+                            <TextField hintText="CorrectAnswer"
+                                fullWidth={true} />
+                        </Row>
+                        <div
+                            className="AddTestView__AvailableTestView">
+                            <Row center="xs">
+                                <Col lg={12}>
+                                    <SelectField
+                                        multiple={true}
+                                        hintText="Choose category"
+                                        value={this.state.value}
+                                        onChange={this.handlerChange}
+                                        fullWidth={true}
+                                    >
+                                        {categories.map(category => (
+                                            <MenuItem
+                                                key={category}
+                                                insetChildren={true}
+                                                value={category}
+                                                primaryText={category}
+                                                styles={style.button}
+                                            />
+                                        ))}
+                                    </SelectField>
+                                </Col>
+                            </Row>
+                        </div>
+                        <Row center="xs">
+                            <RaisedButton
+                                label="Save"
+                                primary={true}
+                                fullWidth={true}
+                                style={style.button}
+                            />
+                        </Row>
+                    </div>
+                </Grid>
+            </Paper>)
     }
 }
 export default AddTestView
