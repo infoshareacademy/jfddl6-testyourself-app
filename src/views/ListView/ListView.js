44e3ee6c87e275918c6661b521474e1c8d07b9b6
Andrzej Rokosz <andrzej_ro@wp.pl>
2018-11-26 23:25:24 +0100

    wip

--- src/views/ListView/ListView.js
+++ src/views/ListView/ListView.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import SearchView from './SearchView/SearchView'
 
-
 import { database } from './../../firebase'
-
-import { data } from './tempData'
 import MyList from './MyList'
-import Test from './../../Test'
 
 class ListView extends React.Component {
-
     state =
         {
             tests: {}
         }
-
-
 
     componentDidMount() {
         database.ref().child('tests')
@@ -23,16 +16,13 @@ class ListView extends React.Component {
                 this.setState({ tests: snapshot.val() })
                 // console.log(this.state.tests)
             })
-
     }
-
 
     render() {
         return (
             <div>
                 <SearchView />
                 <MyList
-
                     tests={this.state.tests}
                 />
             </div>
--- src/views/ListView/MyList.js
+++ src/views/ListView/MyList.js
@@ -3,48 +3,30 @@ import React from 'react'
 import { List, ListItem } from 'material-ui/List';
 import Avatar from 'material-ui/Avatar';
 import Subheader from 'material-ui/Subheader';
-import { data } from './tempData'
+
 
 const MyList = (props) => (
 
-
     <List>
-        {
-            <div>
+              
                 {console.log(props.tests)}
-
                 < Subheader > Available Tests</Subheader>
+    
                 {
                     props.tests &&
                     props.tests.map &&
                     props.tests
-                        .map(test => ( 
-                            
+                        .map(test => (
                             <ListItem
                                 // key={}
-                                primaryText={test.description}
+                                primaryText={props.tests}
                                 leftAvatar={<Avatar src="" />}
                             />
                         ))
                 }
-            </div>
-
-
-
-        }
-
-
-
+        
     </List >
-
-
 )
 
 export default MyList
 
-
-// props.tests.map(test =>
-//     (<ListItem
-//         primaryText={test.category}
-//         leftAvatar={<Avatar src="" />}
-//     />))
