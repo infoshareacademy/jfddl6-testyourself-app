452f2a9582c16015ffa310f6cb51bc3e1ab4ac14
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-11-27 16:12:17 +0100

    create PieChart component

---
+++ src/views/DashboardView/PieChart.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { PieChart, Pie, Tooltip } from "recharts";
+
+
+const data=[
+    {
+    value:35,
+    name:'new added tests'
+},
+{
+    value:40,
+    name:'internal tests'
+}
+]
