58454270b3e09aaed509cd9632cc81b4f02407a4
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-11-28 16:36:50 +0100

    add responsiveness

--- src/views/DashboardView/BarChart.js
+++ src/views/DashboardView/BarChart.js
@@ -21,13 +21,19 @@ const data = [
 ]
 const Chart = (props) => (
     <div>
-        <BarChart width={600} height={300} data={data}>
+        <BarChart
+            width={props.viewportWidth <= 992 ?
+                props.viewportWidth / 1.5
+                :
+                props.viewportWidth / 2.5
+            }
+            height={300} data={data}>
             <XAxis dataKey="time" stroke="#8884d8" />
             <YAxis />
             <Tooltip wrapperStyle={{ width: 100, backgroundColor: '#ccc' }} />
             <Legend width={100} wrapperStyle={{ top: 40, right: 20, backgroundColor: '#f5f5f5', border: '1px solid #d5d5d5', borderRadius: 3, lineHeight: '40px' }} />
             <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-            <Bar type="monotone" dataKey="users" fill="#8884d8"/>
+            <Bar type="monotone" dataKey="users" fill="#8884d8" />
         </BarChart>
     </div>
 );
