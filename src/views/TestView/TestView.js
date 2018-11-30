a4dfe7ffd76c2958972b2f1e52f1b300d5932447
Andrzej Rokosz <andrzej_ro@wp.pl>
2018-11-30 15:37:19 +0100

    wip

--- src/views/TestView/TestView.js
+++ src/views/TestView/TestView.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { Paper, RaisedButton } from 'material-ui';
 
+const style = {
+    paper: {
+        margin: 20,
+        padding: 20
+    },
+    button:{
+        display:'fixed',
+        position:'bottom'
+    }
+
+}
 
 const TestView = (props) => (
-    <div>
-        TestView
-    </div>
+    <Paper
+    style={
+        style.paper
+    }
+    
+    >
+    TestView
+    <RaisedButton
+    label='Add to favourite'
+    primary={true}
+    style={style.button}
+    />
+        
+    </Paper>
+
 )
 
 export default TestView
