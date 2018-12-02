689de52f206c87cdfa8c0fdaec417a51fc5ca985
Andrzej Rokosz <andrzej_ro@wp.pl>
2018-12-02 20:55:32 +0100

    added image and some info to testView

--- src/views/TestView/TestView.js
+++ src/views/TestView/TestView.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Paper from 'material-ui/Paper'
 import RaisedButton from 'material-ui/RaisedButton'
-import {database} from '../../firebase'
+import { database } from '../../firebase'
 const style = {
     paper: {
         margin: 20,
@@ -13,6 +13,9 @@ const style = {
         left: 0,
         padding: 3,
         bottom: 0
+    },
+    image:{
+        width:'70vw',
     }
 }
 
@@ -20,41 +23,74 @@ class TestView extends React.Component {
     constructor(props) {
         super()
         this.state.id = props.match.params.id
-        this.state.test = {}
+        console.log(props.match.params.id)
 
     }
-    
-   
+    state = {
+        id: '',
+        favorite: null,
+        test: {}
+
+    }
 
     componentDidMount() {
-      database.ref(`/tests/${this.state.id}`).on(
+        database.ref(`/tests/${this.state.id}`).once(
             'value',
-            snapshot=>
+            snapshot => {
+                console.log('this.state.id', this.state.id)
+                console.log('favorite from db', snapshot.val().favorite)
+                this.setState({ test: snapshot.val() })
+                console.log('test state from snap', this.state.test)
+                console.log('test type', typeof (this.state.test))
+                return this.setState({ favorite: snapshot.val().favorite })
+            }
+
         )
-   }
+    }
+
+    componentWillUnmount() {
+        database.ref(`/tests/${this.state.id}`).off()
+    }
 
     onClickHandler = () => {
+        if (this.state.favorite === false) {
+            this.setState({ favorite: true })
+            console.log('test', this.state.id)
+            console.log('www', this.state.test.favorite)
 
+        }
+
+        else
+            console.log('not favorite')
     }
 
-    render() { 
-        
+    render() {
         return (
-       
-        <Paper
-            style={style.paper}
-        >
-            <RaisedButton
-                label={"Add to favourite"}
-                primary={true}
-                style={style.button}
-                fullWidth={true}
-                onClick={() => this.onClickHandler}
-            />
-        </Paper>)
+
+            <Paper
+                style={style.paper}
+            >     
+                <img 
+                src={this.state.test.img}
+                style={style.image}
+                />
+                <h1>{`Test name: ${this.state.test.description}`}</h1>
+                <h2>{`Category: ${this.state.test.category}`}</h2>
+                <h2>{`Number of questions: `}</h2>
+                <h2>{`Difficulty: `}</h2>
+                <h3>{`Description: `}</h3>
+
+                <RaisedButton
+                    label={"Add to favourite"}
+                    primary={true}
+                    style={style.button}
+                    fullWidth={true}
+                    onClick={this.onClickHandler}
+                />
+            </Paper>)
     }
 }
-// console.log(props.match.params.id)
+
 
 
 export default TestView
