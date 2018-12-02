78bdd7902b7abf794dc837bfe8bfbc3aaeece916
Andrzej Rokosz <andrzej_ro@wp.pl>
2018-12-02 17:55:52 +0100

    firebase map fun

--- src/views/TestView/TestView.js
+++ src/views/TestView/TestView.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Paper from 'material-ui/Paper'
 import RaisedButton from 'material-ui/RaisedButton'
-// import {database} from '../../firebase'
+import {database} from '../../firebase'
 const style = {
     paper: {
         margin: 20,
