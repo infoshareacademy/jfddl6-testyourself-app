27416a5b85cb8617d069d96cdeed5cb81857673f
Andrzej Rokosz <andrzej_ro@wp.pl>
2018-12-02 22:35:02 +0100

    wip

--- src/views/TestView/TestView.js
+++ src/views/TestView/TestView.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Paper from 'material-ui/Paper'
 import RaisedButton from 'material-ui/RaisedButton'
 import { database } from '../../firebase'
+// import mapObjectToArray from './../utils/mapObjectToArray'
 
 
 const style = {
@@ -16,14 +17,14 @@ const style = {
         padding: 3,
         bottom: 0
     },
-    image:{
-        width:'70vw',
-        maxWidth:'500px'
+    image: {
+        width: '70vw',
+        maxWidth: '500px'
     },
-    textName:{
+    textName: {
 
     },
-    text:{
+    text: {
 
     }
 }
@@ -32,26 +33,27 @@ class TestView extends React.Component {
     constructor(props) {
         super()
         this.state.id = props.match.params.id
-        console.log(props.match.params.id)
+        // console.log(props.match.params.id)
 
     }
     state = {
         id: '',
         favorite: null,
         test: {},
-        testArray:[],
-        numOfQuestions:0
+        testArray: [],
+        numOfQuestions: 0
     }
 
     componentDidMount() {
         database.ref(`/tests/${this.state.id}`).once(
             'value',
             snapshot => {
-                console.log('favorite from db', snapshot.val().favorite)
+                // console.log('favorite from db', snapshot.val().favorite)
                 this.setState({ test: snapshot.val() })
-                console.log('test state from snap', this.state.test)
-                this.setState({testArray: Object.values(this.state.test)})
-                this.setState({numOfQuestions: Object.values(this.state.testArray[4]).length})
+                // console.log('test state from snap', this.state.test)
+                this.setState({ testArray: Object.values(this.state.test) })
+                this.setState({ numOfQuestions: Object.values(this.state.testArray[4]).length })
+                // console.log('maped snap', mapObjectToArray(snapshot.val()))
                 return this.setState({ favorite: snapshot.val().favorite })
             }
 
@@ -62,15 +64,14 @@ class TestView extends React.Component {
         database.ref(`/tests/${this.state.id}`).off()
     }
 
+   
+
     onClickHandler = () => {
         if (this.state.favorite === false) {
             this.setState({ favorite: true })
-         
-
         }
-
         else
-            console.log('not favorite')
+            this.setState({ favorite: false })
     }
 
     render() {
@@ -78,25 +79,30 @@ class TestView extends React.Component {
 
             <Paper
                 style={style.paper}
-            >     
-                <img 
-                src={this.state.test.img}
-                style={style.image}
-                alt=''
+            >
+                <img
+                    src={this.state.test.img}
+                    style={style.image}
+                    alt=''
                 />
                 <h1 style={style.textName}>{`#Test name: ${this.state.test.description}`}</h1>
                 <h3 style={style.text}>{`Category: ${this.state.test.category}`}</h3>
                 <h3 style={style.text}>{`Number of questions: ${this.state.numOfQuestions}`}</h3>
                 {/* <h3 style={style.text}>{`Difficulty: `}</h3> */}
-                <h3 style={style.text}>{`Description: `}</h3>
+                {/* <h3 style={style.text}>{`Description: `}</h3> */}
 
                 <RaisedButton
-                    label={"Add to favourite"}
+
+                    label={this.state.favorite === true ?
+                        "Remove from favourite"
+                        :
+                        "Add to favourite"}
                     primary={true}
                     style={style.button}
                     fullWidth={true}
                     onClick={this.onClickHandler}
                 />
+                
             </Paper>)
     }
 }
