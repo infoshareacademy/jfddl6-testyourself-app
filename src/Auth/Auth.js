1e51b919db469ab5d3826e3062da7c28d34aeb68
Karol szykula <karol.szykula@gmail.com>
2018-12-04 16:46:37 +0100

    Add auth.onAuthStateChanged function in componentDidMount

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -9,10 +9,39 @@ class Auth extends React.Component {
     state = {
         email: '',
         password: '',
-        isUserLoggedIn: true
+        isUserLoggedIn: false
+    }
+
+    componentDidMount() {
+        auth.onAuthStateChanged(
+            user => {
+                if (user) {
+                    this.setState({ isUserLoggedIn: true })
+                } else {
+                    this.setState({ isUserLoggedIn: false })
+                }
+            }
+        )
+    }
+
+    onEmailChangeHandler = (event) => {
+        this.setState({ email: event.target.value })
+    }
+
+    onPasswordChangeHandler = (event) => {
+        this.setState({ password: event.target.value })
+    }
+
+    onLogInClick = () => {
+
+    }
+
+    onPasswordChangeHandler = () => {
+
     }
 
     render() {
+        console.log(`${this.state.password} ${this.state.email}`)
         return (
             <div>
                 {
@@ -20,9 +49,14 @@ class Auth extends React.Component {
                         ?
                         this.props.children
                         :
-                        <LoginForm>
-
-                        </LoginForm>
+                        <LoginForm
+                            email={this.state.email}
+                            password={this.state.password}
+                            onEmailChangeHandler={this.onEmailChangeHandler}
+                            onPasswordChangeHandler={this.onPasswordChangeHandler}
+                            onLogInClick={this.onLogInClick}
+                            onLogInByGoogleClick={this.onLogInByGoogleClick}
+                        />
                 }
             </div>
         )
