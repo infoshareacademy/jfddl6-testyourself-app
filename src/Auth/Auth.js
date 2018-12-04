98885ab2658d5b1ff6d793b3471362c5b9cfbf30
Karol szykula <karol.szykula@gmail.com>
2018-12-04 16:07:24 +0100

    Add login form

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -2,11 +2,22 @@ import React from 'react'
 
 import { auth, googleProvider } from './../firebase'
 
+import LoginForm from './LoginForm'
+
 class Auth extends React.Component {
+
+    state = {
+        email: '',
+        password: '',
+        isUserLoggedIn: false
+    }
+
     render() {
         return (
             <div>
+                <LoginForm>
 
+                </LoginForm>
             </div>
         )
     }
