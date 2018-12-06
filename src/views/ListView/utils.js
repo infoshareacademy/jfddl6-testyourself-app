853292ce33510150e17e9a3007f6f65e197fa76b
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-12-06 16:23:36 +0100

    add function maps an object into array of objects and and puts object keys into key property of array items

--- src/views/ListView/utils.js
+++ src/views/ListView/utils.js
@@ -5,3 +5,13 @@ export const unifyString = string => (
     .normalize('NFD')
     .replace(/[\u0300-\u036f]/g, "")
 )
+
+export const mapObjectToArray = (obj) => (
+  Object.entries(obj || {})
+    .map(([key, value]) => (
+      typeof value === 'object' ?
+        { ...value, key }
+        :
+        { key, value }
+    ))
+)
