c00e5d2db67f8ce6466ab401430c0fddab6044a3
Andrzej Rokosz <andrzej_ro@wp.pl>
2018-12-11 17:47:21 +0100

    tests duck

---
+++ src/state/tests.js
@@ -0,0 +1,46 @@
+import { database } from '../firebase'
+
+const SET_DATA = 'tests/SET_DATA'
+
+const INITIAL_STATE = {
+    tests: null
+}
+
+export const loadData = (data) => ({
+    type: SET_DATA,
+    data
+})
+
+
+export const loadDataAsyncAction = () => (dispatch, getState) => {
+    database.ref(`/tests`).on(
+        'value',
+        snapshot => {
+            if (!snapshot.val()) {
+                dispatch(loadData([]))
+                // this.setState({ tests: [] })
+                return
+            }
+            const testsArray = Object.entries(snapshot.val())
+            const testList = testsArray.map(([id, values]) => {
+                values.id = id
+                return values
+            })
+            dispatch(loadData(testList))
+
+            // this.setState({ tests: testList })
+        }
+    )
+}
+
+export default (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case SET_DATA:
+            return {
+                ...state,
+                tests: action.data
+            }
+        default:
+            return state
+    }
+}
