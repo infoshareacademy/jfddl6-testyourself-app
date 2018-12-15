7691f7d17b15b8b3fac8d043680566eb6be99406
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-12-15 21:13:14 +0100

    modify pieChart

--- src/views/DashboardView/DashboardView.js
+++ src/views/DashboardView/DashboardView.js
@@ -5,8 +5,8 @@ import { Grid, Row, Col } from 'react-flexbox-grid'
 import { Link } from 'react-router-dom'
 import PieChart from './PieChart'
 import BarChart from './BarChart'
-import { database } from '../../firebase'
-import { mapObjectToArray } from './utils'
+//import { database } from '../../firebase'
+import {connect}from 'react-redux'
 
 const style = {
     paper: {
@@ -15,14 +15,15 @@ const style = {
     }
 }
 
-const dbMessagesRef = database.ref('/usersLogins')
-console.log(dbMessagesRef)
+// const dbMessagesRef = database.ref('/tests')
+// console.log(dbMessagesRef)
 
 
 class DashboardView extends React.Component {
     state = {
         viewportWidth: window.innerWidth,
         timeStamps: [],
+        tests:[]
 
     }
 
@@ -32,15 +33,22 @@ class DashboardView extends React.Component {
             this.resizeListener
         )
 
-        dbMessagesRef.on(
-            'value',
-            snapshot => {
-                console.log(snapshot.val())
-                this.setState({
-                    tests: mapObjectToArray(snapshot.val())
-                })
-            }
-        )
+        // database.ref(`/tests`).on(
+        //     'value',
+        //     snapshot => {
+        //         if (!snapshot.val()) {
+        //             this.setState({ tests: [] })
+        //             return
+        //         }
+        //         const allTestsArray = Object.entries(snapshot.val())
+        //         const testList = allTestsArray.map(([id, values]) => {
+        //             values.id = id
+        //             return values
+                    
+        //         })
+        //         this.setState({ tests: testList })
+        //     }
+        // )
     }
 
 
@@ -52,6 +60,82 @@ class DashboardView extends React.Component {
     dataChangeHandler = event => this.setState({ newTests: event.target.value })
 
     render() {
+        const now = new Date()
+        const todayMidnightTimestamp = now.getTime()
+            - now.getHours() * 60 * 60 * 1000
+            - now.getMinutes() * 60 * 1000
+            - now.getSeconds() * 1000
+            - now.getMilliseconds()
+
+        const loginsData = this.props._loginsData && this.props._loginsData.map(timestamp => timestamp.timestamp)
+
+        const midnightTimestamp = {
+            day0: todayMidnightTimestamp,
+            day1: todayMidnightTimestamp - 24 * 60 * 60 * 1000,
+            day2: todayMidnightTimestamp - 2 * 24 * 60 * 60 * 1000,
+            day3: todayMidnightTimestamp - 3 * 24 * 60 * 60 * 1000,
+            day4: todayMidnightTimestamp - 4 * 24 * 60 * 60 * 1000,
+            day5: todayMidnightTimestamp - 5 * 24 * 60 * 60 * 1000,
+            day6: todayMidnightTimestamp - 6 * 24 * 60 * 60 * 1000
+        }
+
+        const loginsPerDay = {
+            day0: loginsData && loginsData.filter(timestamp => timestamp >= midnightTimestamp.day0).length,
+            day1: loginsData && loginsData.filter(timestamp => timestamp >= midnightTimestamp.day1 && timestamp < midnightTimestamp.day0).length,
+            day2: loginsData && loginsData.filter(timestamp => timestamp >= midnightTimestamp.day2 && timestamp < midnightTimestamp.day1).length,
+            day3: loginsData && loginsData.filter(timestamp => timestamp >= midnightTimestamp.day3 && timestamp < midnightTimestamp.day2).length,
+            day4: loginsData && loginsData.filter(timestamp => timestamp >= midnightTimestamp.day4 && timestamp < midnightTimestamp.day3).length,
+            day5: loginsData && loginsData.filter(timestamp => timestamp >= midnightTimestamp.day5 && timestamp < midnightTimestamp.day4).length,
+            day6: loginsData && loginsData.filter(timestamp => timestamp >= midnightTimestamp.day6 && timestamp < midnightTimestamp.day5).length
+        }
+
+        const barData = Object.values(loginsPerDay).map((loginsNumber, index) => ({
+            day: 0 - index,
+            participants: loginsNumber,
+            fill: "rgba(229, 80, 57, 1)",
+            label: false
+        }))
+
+        const testsData = this.props._data
+
+        const numberOfTestsScienceComp = testsData.filter(e => e.category === 'Science Computers').length
+        const numbersOfTestsAnimals = testsData.filter(e => e.category === 'Animals').length
+        const numberOfTestsGeography = testsData.filter(e => e.category === 'Geography').length
+        const numberOfTestsMythology = testsData.filter(e => e.category === 'Mythology').length
+
+
+        const pieData = [
+            {
+                value: numberOfTestsScienceComp,
+                name: 'Science Computers',
+                fill: 'blue'
+            },
+            {
+                value: numbersOfTestsAnimals,
+                name: 'Animals',
+                fill: 'red'
+            },
+            {
+                value: numberOfTestsGeography,
+                name: 'Geography',
+                fill: 'purple'
+            },
+            {
+                value: numberOfTestsMythology,
+                name: 'Mythology',
+                fill: 'green'
+            },
+        ]
+
+
+
+
+
+
+
+
+
+
         return (
             <Paper
                 style={style.paper}>
@@ -83,11 +167,7 @@ class DashboardView extends React.Component {
                         <Col lg={6}>
                             <Row middle="xs" center='xs'>
                                 <PieChart
-                                    data={[{
-                                        value: 25,
-                                        name: 'Science Computers',
-                                        fill: 'blue'
-                                    }]}
+                                     pieChartData={pieData}
                                     viewportWidth={this.state.viewportWidth}
                                 />
                             </Row>
@@ -95,6 +175,7 @@ class DashboardView extends React.Component {
                         <Col lg={6}>
                             <Row middle="xs" center='xs'>
                                 <BarChart
+                                barChartData={barData}
                                     viewportWidth={this.state.viewportWidth}
                                 />
                             </Row>
@@ -111,12 +192,20 @@ class DashboardView extends React.Component {
             'resize',
             this.resizeListener
         )
-        dbMessagesRef.off()
+        //dbMessagesRef.off()
     }
 
 }
 
-export default DashboardView
+const mapStateToProps = state => ({
+    _loginsData: state.loginsTimestamps.loginsData,
+    _data: state.favouritesView.data
+})
+
+export default connect(mapStateToProps)(DashboardView)
+
+
+
 
 
 
--- src/views/DashboardView/PieChart.js
+++ src/views/DashboardView/PieChart.js
@@ -1,82 +1,56 @@
 import React from 'react'
-import { PieChart, Pie, Tooltip, Cell } from "recharts";
-import { database } from '../../firebase'
+import { PieChart, Pie, Tooltip } from "recharts";
 
 
+const pieChartData = [
+    {
+        value: 25,
+        name: 'Science Computers',
+        fill: 'blue'
+    },
+    {
+        value: 20,
+        name: 'Animals',
+        fill: 'red'
+    },
+    {
+        value: 15,
+        name: 'Geography',
+        fill: 'purple'
+    },
+    {
+        value: 35,
+        name: 'Mythology',
+        fill: 'green'
+    },
+    {
+        value: 15,
+        name: 'New-added test',
+        fill: 'yellow'
+    }
+]
 
-
-
-const Chart = (props) => {
-
-    const numberOfTestsScienceComp = props.tests.filter(
-        test => test.category === 'Science Computers'
-    )
-
-    const numbersOfTestsAnimals = props.tests.filter(
-        test => test.category === 'Animals'
-    )
-    const numberOfTestsGeography = props.tests.filter(
-        test => test.category === 'Geography'
-    )
-
-    const numberOfTestsMythology = props.tests.filter(
-        test => test.category === 'Mythology'
-    )
-
-
-    const data = [
-        {
-            value: numberOfTestsScienceComp.length,
-            name: 'Science Computers',
-            fill: 'blue'
-        },
-        {
-            value: numbersOfTestsAnimals.length,
-            name: 'Animals',
-            fill: 'red'
-        },
-        {
-            value: numberOfTestsGeography.length,
-            name: 'Geography',
-            fill: 'purple'
-        },
-        {
-            value: numberOfTestsMythology.length,
-            name: 'Mythology',
-            fill: 'green'
+const Chart = (props) => (
+    <div>
+        <h2
+            style={{ textAlign: 'center' }}
+        >
+            Tests categories
+    </h2>
+        <PieChart width={
+            props.viewportWidth <= 992 ?
+                props.viewportWidth / 1.5
+                :
+                props.viewportWidth / 2.5
         }
-        // {
-        //     value: 15,
-        //     name: 'New-added test',
-        //     fill: 'yellow'
-        // }
-    ]
-    const pie_cells = data.map((entry, index) => {
-        return (<Cell key={index} fill={entry.color} />)
-    });
-
-    return (
-        <div>
-            <h3>Number test categories in database</h3>
-            <PieChart width={
-                props.viewportWidth <= 992 ?
-                    props.viewportWidth / 1.5
-                    :
-                    props.viewportWidth / 2.5
-            }
-                height={500}>
-                <Pie
-                    data={props.data || data}
-                    dataKey="value"
-                    nameKey="name"
-                >
-                    {pie_cells}
-                </Pie>
-                <Tooltip />
-            </PieChart>
-        </div>
-    )
-}
-
+            height={500}>
+            <Pie
+                data={props.pieChartData || pieChartData}
+                dataKey="value"
+                nameKey="name" />
+            <Tooltip />
+        </PieChart>
+    </div>
+);
 export default Chart
 
